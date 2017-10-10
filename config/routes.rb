be3bff592a6548b97064d91af4359c9d9c17573d
kazuyuki ikeda <gankai1104@gmail.com>
2017-10-10 13:36:19 +0900

    Add blogs and contacts page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :tops, only: :index
   resources :categories, only: :index
   resources :questions, only: :index
+  resources :contacts, only: %w[new create]
+  resources :blogs, only: %w[index] do
+    collection do
+      get :detail # FIXME: 本稼働の際には、書き直す
+    end
+  end
 
   root 'tops#index'
 end
