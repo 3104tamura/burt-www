0d229b76124a91ad4f6748dfb2c36b558fe65618
kazuyuki ikeda <gankai1104@gmail.com>
2017-10-13 12:09:04 +0900

    Modify href, coding style.. etc

---
+++ app/controllers/training_controller.rb
@@ -0,0 +1,4 @@
+class TrainingController < ApplicationController
+  # FIXME: クラス名も暫定
+  def new; end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, omniauth_providers: [:facebook]
 
-  validates :email, presence: true
-  validates :nickname, :gender, presence: true, on: :new
-  validates :terms_of_service, acceptance: true, on: :new
+  validates :nickname, :gender, presence: true, on: :create
+  validates :terms_of_service, acceptance: true, on: :create
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -16,15 +15,17 @@ class User < ApplicationRecord
       user.first_name = auth.info.first_name
       user.image = auth.info.image
       user.gender = if auth.info.gender == 'male'
-                   0
-                 elsif auth.info.gender == 'female'
-                   1
-                 else
-                   2
+                      0
+                    elsif auth.info.gender == 'female'
+                      1
+                    else
+                      2
                  end
+      # FIXME: 暫定
+      user.terms_of_service = '1'
+      user.nickname = 'facebook認証'
 
       user.password = Devise.friendly_token[0, 20]
-      # user.image = auth.info.image # assuming the user model has an image
     end
   end
 end
