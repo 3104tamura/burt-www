d90e970bb35acedb3e38c94ea2c6d742d72acd83
kazuyuki ikeda <gankai1104@gmail.com>
2017-11-19 22:33:08 +0900

    Fix autocorrect by rubocop

--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TrainingController < ApplicationController
   # FIXME: クラス名も暫定
   def new; end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   resources :tops, only: :index
   resources :categories, only: :index
   resources :questions, only: :index
-  resources :trainer, only: :index
   resources :contacts, only: %w[new create]
   resources :blogs, only: %w[index] do
     collection do
@@ -21,9 +20,12 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' },
-                     controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "registrations"}, skip: %w[session logout password regstration]
+  devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout' },
+                     controllers: { sessions: 'users/sessions', registrations: "users/registrations"}, skip: %w[logout password]
 
+  devise_scope :user do
+    get "/auth/:provider/callback" => "authentications#create"
+  end
   as :user do
     get 'sign_up' => 'devise/regstrations#new'
     post 'sign_up' => 'devise/regstrations#create'
@@ -37,5 +39,5 @@ Rails.application.routes.draw do
 
   root 'tops#index'
 
-  match '*not_found' => 'application#routing_error', via: %w[get post]
+  # match '*not_found' => 'application#routing_error', via: %w[get post]
 end
